b943664d4a4363164edd35308ebedc48348f7034
nanda.kumar.office1@gmail.com <nanda.kumar.office1@gmail.com>
2023-04-10 18:51:06 +0530

    upgrade script

---
+++ linux/upgrade_nirvashare.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+DOCKER_FILE="/var/nirvashare/install-app.yml"
+
+terminate()
+{
+    echo ""
+    echo "Upgrade terminated"
+    exit 1
+}
+
+update_configuration()
+{
+    echo "Updating configuration"
+    sudo curl -L "https://raw.githubusercontent.com/nirvashare/nirvashare/main/docker/common/install-app.yml" -o /var/nirvashare/install_file
+    line=$(grep -m1 "ns_db_password" $DOCKER_FILE)
+    NS_PASSWORD=`echo "$line" | cut -d'"' -f 2`
+    mv $DOCKER_FILE ${DOCKER_FILE}_del
+    cat /var/nirvashare/install_file  | sed -e "s/__DB_PASS__/$NS_PASSWORD/" > $DOCKER_FILE
+}
+
+update_nirvashare()
+{
+    echo "Updating application"
+    docker-compose -f $DOCKER_FILE pull
+    docker-compose -f $DOCKER_FILE up -d
+}
+
+
+
+echo ""
+echo "NirvaShare Upgrade Utility."
+echo ""
+
+echo "This will upgrade NirvaShare applications to latest version."
+read -p "Do you want to continue? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+   terminate;
+fi
+
+echo ""
+echo ""
+
+
+if [ -f "$DOCKER_FILE" ]; then
+    #check if install file has search feature.
+    if ! grep -q nirvashare_search "$DOCKER_FILE"; then
+        update_configuration
+    fi
+else 
+    echo "$DOCKER_FILE does not exist."
+    terminate;
+fi
+
+
+
+
+
+update_nirvashare
+#docker-compose -f /var/nirvashare/install-app.yml up -d
+
+echo ""
+echo "Installation Completed."
+
+
