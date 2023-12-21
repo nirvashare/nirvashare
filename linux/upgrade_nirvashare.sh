ec43796ca3b4d77ccd650651f294e1bf8af6db2c
Nandakumar <nanda.kumar.office1@gmail.com>
2023-12-21 16:29:08 +0530

    fixed spelling mistake

--- linux/upgrade_nirvashare.sh
+++ linux/upgrade_nirvashare.sh
@@ -72,7 +72,7 @@ cleanup
 echo ""
 echo "Upgrade Completed Successfully!"
 echo ""
-echo "NOTE - Please wait for couple minutes for the services to start automatically. Warning on Orphan containers can be ignored. "
+echo "NOTE - Please wait for couple of minutes for the services to start automatically. Warning on Orphan containers can be ignored. "
 echo ""
 
 
