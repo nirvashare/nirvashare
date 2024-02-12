81a0ea0ddb7888cd725f005b44497a03b675e313
nanda.kumar.office1@gmail.com <nanda.kumar.office1@gmail.com>
2024-02-12 07:42:27 +0530

    upgrade script

--- linux/upgrade_nirvashare.sh
+++ linux/upgrade_nirvashare.sh
@@ -2,12 +2,33 @@
 
 
 DOCKER_FILE="/var/nirvashare/install-app.yml"
+DOCKER_FILE_FTPS="/var/nirvashare/install-ftps.yml"
+DB_PASS_FILE=/var/nirvashare/dbpass
 
 terminate()
 {
     echo ""
     echo "Upgrade terminated"
     exit 1
+}
+
+create_pass_file()
+{
+
+    if [ -f "$DB_PASS_FILE" ]; then
+    	echo "Password file already exists"
+    	terminate; exit;
+    else 
+    	# create the password file
+        echo $NS_DBPASSWORD > ${DB_PASS_FILE}    
+    fi
+}
+
+update_docker_compose()
+{
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
 }
 
 cleanup()
@@ -19,52 +40,68 @@ cleanup()
 update_configuration()
 {
     echo "Updating configuration"
-    sudo curl -L "https://raw.githubusercontent.com/nirvashare/nirvashare/main/docker/common/install-app.yml" -o /var/nirvashare/install_file
+
     line=$(grep -m1 "ns_db_password" $DOCKER_FILE)
-    NS_PASSWORD=`echo "$line" | cut -d'"' -f 2`
-    mv $DOCKER_FILE ${DOCKER_FILE}.del
-    cat /var/nirvashare/install_file  | sed -e "s/__DB_PASS__/$NS_PASSWORD/" > $DOCKER_FILE
+    NS_DBPASSWORD=`echo "$line" | cut -d'"' -f 2`
+    mv $DOCKER_FILE ${DOCKER_FILE}_1.del
+    sudo curl -L "https://raw.githubusercontent.com/nirvashare/nirvashare/main/docker/common/install-app.yml" -o ${DOCKER_FILE}
+
+    create_pass_file
 }
 
 update_nirvashare()
 {
     echo "Updating applications"
     echo ""
+    export COMPOSE_IGNORE_ORPHANS=true
+    
     docker-compose -f $DOCKER_FILE pull
     docker-compose -f $DOCKER_FILE up -d
+    
+    if [ -e "$DOCKER_FILE_FTPS" ]; then
+        docker-compose -f $DOCKER_FILE_FTPS pull
+        docker-compose -f $DOCKER_FILE_FTPS up -d
+    fi
 }
 
 
+user_prompt()
+{
 
-echo ""
-echo "NirvaShare Upgrade Utility."
-echo ""
+	echo ""
+	echo "NirvaShare Upgrade Utility."
+	echo ""
 
-echo "This will upgrade NirvaShare applications to latest version."
-while true; do
-    read -p "Do you want to continue? (y/n)? " yn
-    case $yn in
-        [Yy] ) break;;
-        [Nn] ) terminate; exit;;
-        * ) echo "Please answer yes or no (y/n).";;
-    esac
-done
+	echo "This will upgrade NirvaShare applications to latest version."
+	while true; do
+	    read -p "Do you want to continue? (y/n)? " yn
+	    case $yn in
+		[Yy] ) break;;
+		[Nn] ) terminate; exit;;
+		* ) echo "Please answer yes or no (y/n).";;
+	    esac
+	done
 
-echo ""
-echo ""
+	echo ""
+	echo ""
+}
 
-if [ -f "$DOCKER_FILE" ]; then
-    #check if install file has search feature.
-    if ! grep -q nirvashare_search "$DOCKER_FILE"; then
-        update_configuration
+update_config() {
+    if [ -f "$DOCKER_FILE" ]; then
+        #check if install file has search feature.
+        if [ ! -e "$DB_PASS_FILE" ]; then
+         update_configuration
+        fi
+    else 
+        echo "$DOCKER_FILE does not exist."
+        terminate;
     fi
-else 
-    echo "$DOCKER_FILE does not exist."
-    terminate;
-fi
+}
 
 
-
+user_prompt
+update_docker_compose
+update_config
 update_nirvashare
 cleanup
 
