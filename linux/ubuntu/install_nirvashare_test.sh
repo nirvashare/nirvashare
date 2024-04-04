a9c5216bf62590bc66fa029cdc6c987a79831f1f
Nandakumar <nanda.kumar.office1@gmail.com>
2024-04-04 18:55:26 +0530

    fixed prompt issue for ubuntu 22

--- linux/ubuntu/install_nirvashare_test.sh
+++ linux/ubuntu/install_nirvashare_test.sh
@@ -89,7 +89,7 @@ sudo apt install -yq apt-transport-https ca-certificates curl software-propertie
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt update
 
