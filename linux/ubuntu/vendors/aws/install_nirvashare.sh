4a4b25d55f85a74498cc79d243749dc4dc188217
Nanda <nanda.kumar.office1@gmail.com>
2025-10-08 08:00:31 +0530

    fixed to use latest docker compose

--- linux/ubuntu/vendors/aws/install_nirvashare.sh
+++ linux/ubuntu/vendors/aws/install_nirvashare.sh
@@ -41,7 +41,8 @@ sudo apt install -yq docker-ce
 
 # docker-compose
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
