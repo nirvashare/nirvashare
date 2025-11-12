f4214c8c8d38e6d2c83c802778e5f8b2c4c7e43b
Nanda <nanda.kumar.office1@gmail.com>
2025-11-12 23:23:41 +0530

    updated docker install

--- linux/ubuntu/vendors/aws/install_nirvashare.sh
+++ linux/ubuntu/vendors/aws/install_nirvashare.sh
@@ -26,18 +26,22 @@ fi
 
 sudo apt update
 # docker
+    echo "Updating package list..."
+    sudo apt update -y
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-sudo apt install -yq apt-transport-https ca-certificates curl software-properties-common
+    echo "Adding Docker’s official GPG key..."
+    curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    echo "Adding Docker repository..."
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    echo "Installing Docker..."
+    sudo apt update -y
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo apt update
-
-apt-cache policy docker-ce
-
-sudo apt install -yq docker-ce
+    echo "Enabling and starting Docker..."
+    sudo systemctl enable --now docker
 
 # docker-compose
 
